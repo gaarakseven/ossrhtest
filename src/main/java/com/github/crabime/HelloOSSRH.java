de71c396a015b269de9af7a1530c50cd75aa4354
crabime <crabime@gmail.com>
2018-12-24 11:13:47 +0800

    test deploy jar files to remote

---
+++ src/main/java/com/github/crabime/HelloOSSRH.java
@@ -0,0 +1,8 @@
+package com.github.crabime;
+
+public class HelloOSSRH {
+
+    public static void main(String[] args) {
+        System.out.println("The first time using ossrh to deploy jar file to remote");
+    }
+}
